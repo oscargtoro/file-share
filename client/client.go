79a80473e4ac4de3e5c492cc1220960f3ccaa37b
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2022-02-15 09:53:47 -0500

    Added function send
    Sends a request to join a channel with the host and the port is going to start listening for files. Right now copies files to the path ./.

--- client/client.go
+++ client/client.go
@@ -7,8 +7,14 @@ import (
 	"log"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
+)
+
+const (
+	HOST = "localhost"
+	PORT = "8081"
 )
 
 // Send creates a buffer for the body, then using that buffer creates a multipart
@@ -63,6 +69,51 @@ func send(path string) {
 	fmt.Println(string(response))
 }
 
+// join Sends a request to join a channel with the host and the port is going
+// to start listening for files. Right now copies files to the path ./.
+func join(channel string) {
+	data := url.Values{
+		"name": {channel},
+		"host": {HOST},
+		"port": {PORT},
+	}
+	res, err := http.PostForm("http://localhost:8080/join", data)
+	if err != nil {
+		fmt.Println("Could not connect to http://localhost:8080")
+	}
+	body, err := io.ReadAll(res.Body)
+	fmt.Println(string(body))
+	sendHandler := func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseMultipartForm(32 << 20) // Max 32MB
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		file, fHeader, err := r.FormFile("file")
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		tmpfile, err := os.Create("./" + fHeader.Filename)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer tmpfile.Close()
+		_, err = io.Copy(tmpfile, file)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("File Recieved"))
+	}
+	http.HandleFunc("/send", sendHandler)
+	log.Println("Listening on port " + PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("Not enough arguments")
@@ -70,6 +121,10 @@ func main() {
 	}
 	if os.Args[1] == "send" {
 		send(os.Args[2])
+		os.Exit(0)
+	}
+	if os.Args[1] == "join" {
+		join(os.Args[2])
 		os.Exit(0)
 	}
 }
