1f57bb1da607cd81ed34a44ead6a4bf40a4d4fd0
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2022-02-15 09:57:39 -0500

    Added handler for a join channel request
    Receives join request and store the connection information of the client

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,7 +13,25 @@ const (
 	PORT = "8080"
 )
 
-func main() {
+func start(channels map[string][]string) {
+
+	// handler function for /join, logic for when channel registration is issued
+	joinHandler := func(w http.ResponseWriter, r *http.Request) {
+		channel := r.FormValue("name")
+		log.Println("Request to join channel " + channel + " recieved...")
+		if _, ok := channels[channel]; !ok {
+			channels[channel] = append(channels[channel], r.FormValue("host"))
+			channels[channel] = append(channels[channel], r.FormValue("port"))
+		}
+		/* client := new(strings.Builder)
+		log.Fprint(client, clientPort)
+		log.Println(clientPort)
+		clients[string(clientPort)] = name */
+		io.WriteString(w, "Registered in channel "+channel)
+		log.Println(channels)
+	}
+
+	// handler function for /send, logic for when a file share request is issued
 	sendHandler := func(w http.ResponseWriter, r *http.Request) {
 		err := r.ParseMultipartForm(32 << 20) // Max 32MB
 		if err != nil {
@@ -40,7 +59,21 @@ func main() {
 		w.Write([]byte("File Recieved"))
 	}
 
+	http.HandleFunc("/join", joinHandler)
 	http.HandleFunc("/send", sendHandler)
 	log.Println("Listening on port " + PORT)
 	log.Fatal(http.ListenAndServe(":"+PORT, nil))
+}
+
+func main() {
+	channels := make(map[string][]string)
+	if len(os.Args) == 1 {
+		fmt.Println("Not enough arguments")
+		os.Exit(0)
+	}
+	if os.Args[1] == "start" {
+		start(channels)
+	} else {
+		fmt.Println("Invalid Argument")
+	}
 }
