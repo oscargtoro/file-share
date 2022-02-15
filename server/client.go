da560b2104a45067d069aa10304ea94c3325359d
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2022-02-15 09:02:07 -0500

    Handles a client request to send a file
    Right now just copies the file to current location

--- server/client.go
+++ server/client.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
+const (
+	HOST = "localhost"
+	PORT = "8080"
+)
+
+func main() {
+	sendHandler := func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseMultipartForm(32 << 20) // Max 32MB
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		file, fHeader, err := r.FormFile("file")
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		tmpfile, err := os.Create("./" + fHeader.Filename)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer tmpfile.Close()
+		_, err = io.Copy(tmpfile, file)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("File Recieved"))
+	}
+
+	http.HandleFunc("/send", sendHandler)
+	log.Println("Listening on port " + PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
